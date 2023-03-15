ef4de589dabf56dcb371cc17cf29cc995daa9a1c
Lance <lmreineer@gmail.com>
2023-03-15 09:08:50 +0800

    Fix image bug

--- docs/script/hourly-daily/hourlyDetails.js
+++ docs/script/hourly-daily/hourlyDetails.js
@@ -26,7 +26,7 @@ function getHour(hoursDisplayed) {
 
 function getIcon(hoursDisplayed) {
   const { icon } = hoursDisplayed;
-  return `res/icon-set/${icon}.svg`;
+  return `./res/icon-set/${icon}.svg`;
 }
 
 function getCelciusTemp(hoursDisplayed) {
